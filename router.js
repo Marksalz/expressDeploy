4756bab6726498deff940b098e1340ebe07969e6
Mark <menachemsalzberg13@gmail.com>
2025-07-09 11:47:17 +0300

    added file directory

--- router.js
+++ router.js
@@ -1,16 +1,28 @@
 import express from "express";
 import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const router = express.Router();
+
+// Get __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dataFilePath = path.join(__dirname, "file.txt");
 
 router.get("/", (req, res) => res.send("hello"));
 
 router.post("/create", async (req, res) => {
     try {
-        const fileData = await fs.readFile("file.txt", "utf8");
-        const data = JSON.parse(fileData);
-        if (!data) res.status(500).send("Server internal error");
+        let data = [];
+        try {
+            const fileData = await fs.readFile(dataFilePath, "utf8");
+            data = JSON.parse(fileData);
+        } catch (err) {
+            if (err.code !== "ENOENT") throw err;
+        }
         data.push(req.body);
-        await fs.writeFile("file.txt", JSON.stringify(data));
+        await fs.writeFile(dataFilePath, JSON.stringify(data));
         res.status(201).send("success");
     } catch (err) {
         res.status(err.status || 500).send(err.message || "Server internal error");
